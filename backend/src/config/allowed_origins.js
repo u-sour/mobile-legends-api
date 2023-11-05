d382cd07d264e00927e26dad8702826c2f2fff52
u-sour <yousour.dev@gmail.com>
2023-11-05 13:57:48 +0700

    deploy x2

--- backend/src/config/allowed_origins.js
+++ backend/src/config/allowed_origins.js
@@ -4,7 +4,8 @@ const origins = [
   "http://localhost:5173",
   "http://192.168.1.13:5173",
   "http://localhost:5174",
-  "http://192.168.1.13:5174"
+  "http://192.168.1.13:5174",
+  "https://mlbb-api.onrender.com"
 ];
 
 export default origins;
