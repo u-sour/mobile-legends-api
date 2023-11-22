cbc9ff05456daf0aaafc9356ea2ffad504b69679
u-sour <yousour.dev@gmail.com>
2023-11-22 09:23:55 +0700

    add vercel.json for deployment

--- backend/src/config/allowed_origins.js
+++ backend/src/config/allowed_origins.js
@@ -5,7 +5,8 @@ const origins = [
   "http://192.168.1.13:5173",
   "http://localhost:5174",
   "http://192.168.1.13:5174",
-  "https://mlbb-api.onrender.com"
+  "https://mlbb-api.onrender.com",
+  "https://mlbb-api.vercel.app"
 ];
 
 export default origins;
